49e9b32634d92b9a300289f55cacb1d289948092
Pradeep Kumar Gupta ( Alven Alex ) <MRYASHFTP@GMAIL.COM>
2026-06-16 21:03:46 +0530

    Create sketch.ino

---
+++ IOT/LCD(16x2)/sketch.ino
@@ -0,0 +1,20 @@
+
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+  lcd.begin(16, 2); 
+ 
+}
+
+void loop()
+{
+
+  lcd.setCursor(2, 0);
+  lcd.print("Hello world!");
+  delay(1000); 
+
+}
